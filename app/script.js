b27dda9eac934fa99bd3dedf6b594864e3b7fb4a
gayatridevik-mediwave <gayatridevi_k@mindwaveventures.com>
2022-01-11 15:46:28 +0530

    Added skeleton and css for movie list and items

--- app/script.js
+++ app/script.js
@@ -1,1 +1,22 @@
-console.log('Hello world!')
+// // default parameters
+// function sayHello(name = "world") {
+//   //   if (!name) {
+//   //     name = "world";
+//   //   }
+//   console.log("hello " + name);
+// }
+
+// /*
+// document.querySelector('.ola-btn') // class - can be many
+// document.querySelector('#hello-btn') // id - can only be one in document
+// */
+
+// const helloBtn = document.querySelector("#hello-btn");
+// const nameInput = document.querySelector("#user-name");
+// helloBtn.addEventListener("click", function () {
+//   const name = nameInput.value;
+//   if (!name) {
+//     return;
+//   }
+//   sayHello(name);
+// });
